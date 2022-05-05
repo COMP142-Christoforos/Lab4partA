b623d77e40155680fe53070054c111804d9b18b0
christoforoschari <christoforos_chari@hotmail.com>
2022-05-05 21:02:34 +0300

    Shapes.cpp create

---
+++ Lab4partA/Shapes.cpp
@@ -0,0 +1,20 @@
+/** \file Shapes.cpp
+* \Library to draw various shapes implimentation.
+* \details This progrma illustates a multifile.
+* \Christoforos Chari
+* \date 18/03/2022
+*/
+#include"Shapes.h"
+#include<iostream>
+
+using namespace std;
+
+/**
+* Fuction <code> drawHorizontalLine <code> draws a horizontal line
+* <BR>
+* @param length The length of the line.
+* @param ch The sympol to use for the line.
+*/
+void drawHorizontalLine(const int length, const char ch) {
+	cout << "\nI am a drawHorizontalLine";
+}
