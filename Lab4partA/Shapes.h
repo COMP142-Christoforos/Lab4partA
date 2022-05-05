16fd4c64843d060f92231197d6143030a0c7b90c
christoforoschari <christoforos_chari@hotmail.com>
2022-05-05 20:59:54 +0300

    move functions prototypes to a Shapes.h

---
+++ Lab4partA/Shapes.h
@@ -0,0 +1,17 @@
+/** \file Shapes.h
+* \brief Header file for a small drawing Library
+* \details This progrma
+* \Christoforos Chari
+* \date 18/03/2022
+*/
+#ifndef SHAPES_H
+#define SHAPES_H
+
+//Prototypes
+void drawHorizontalLine(const int length, const char ch);
+void drawVerticalLine(const int height, const char ch);
+void drawSquare(const int size, const char ch);
+void drawRectangle(const int height, const int length, const char ch);
+void drawRandomShapes(const int numShapes);
+
+#endif  
